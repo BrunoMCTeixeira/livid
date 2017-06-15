7e78e5815cea7844e1627018592f07fd4a2f0205
Bruno Teixeira <agencia.navilla@gmail.com>
2017-06-15 11:52:11 +0100

    Added Users database schema + model factory

---
+++ database/migrations/2017_06_15_104042_update_users_table.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->tinyInteger('=status')->default(0);
+            $table->tinyInteger('role')->default(0);
+            $table->string('username')->unique();
+            $table->string('lastname')->nullable();
+            $table->string('facebookid')->nullable();
+            $table->string('googleid')->nullable();
+            $table->string('gender', 1);
+            $table->date('birthday');
+            $table->string('photo')->nullable();
+            $table->string('cover')->nullable();
+            $table->string('from')->nullable();
+            $table->string('country_id', 2);
+            $table->text('description')->nullable();
+            $table->string('profession')->nullable();
+            $table->string('idioms')->nullable();
+            $table->string('website')->nullable();
+            $table->date('foundationdate')->nullable();
+            $table->string('visitedcountries')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('=status');
+            $table->dropColumn('role');
+            $table->dropColumn('username');
+            $table->dropColumn('lastname');
+            $table->dropColumn('facebookid');
+            $table->dropColumn('googleid');
+            $table->dropColumn('gender');
+            $table->dropColumn('birthday');
+            $table->dropColumn('photo');
+            $table->dropColumn('cover');
+            $table->dropColumn('from');
+            $table->dropColumn('country_id');
+            $table->dropColumn('description');
+            $table->dropColumn('profession');
+            $table->dropColumn('idioms');
+            $table->dropColumn('website');
+            $table->dropColumn('foundationdate');
+            $table->dropColumn('visitedcountries');
+        });
+    }
+}
